aed94a41f2d446ee0299d903b1744e107baa8a54
rickey201xrz <95132501+rickey201xrz@users.noreply.github.com>
2026-09-17 22:30:22 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return "Hello I'm from mango"
+import os
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    while(True){
+        print("Hello World!")
+    }
