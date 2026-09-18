6eec4dc698115e263ce7e71386928b10181de947
rickey201xrz <95132501+rickey201xrz@users.noreply.github.com>
2026-09-18 21:10:08 +0800

    Include system commands in main loop
    Added system commands to print user ID and processes.

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import os
 
 if __name__ == '__main__':
-    os.system("id")
-    os.system("ps -ef")
     while(True):
         print("Hello World!")
+        os.system("id")
+        os.system("ps -ef")
         sleep(10000)
